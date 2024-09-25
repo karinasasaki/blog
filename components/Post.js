2e313be58b2013eb0ae3f694fba8eaf808f9a451
Karina Sasaki <karinasasaki99@gmail.com>
2024-09-25 00:14:40 -0400

    fix: :adhesive_bandage: Deixa o card clicável

--- components/Post.js
+++ components/Post.js
@@ -4,7 +4,7 @@ import styles from "@/styles/components/Post.module.css"
 
 export default function Post() {
   return (
-    <div className={styles.post}>  
+    <Link href={"/post/bolo-de-cenoura"} className={styles.post}>
       <Image
         className={styles.post__foto}
         src="https://i.panelinha.com.br/i1/64-o-1143-blogayu4383.webp"
@@ -12,9 +12,8 @@ export default function Post() {
         width={640}
         height={427}
       />
-      <Link href={"/post/bolo-de-cenoura"}>
-        <h1>Bolo de cenoura</h1>
-      </Link>
-    </div>
+      <h1>Bolo de cenoura</h1>
+      <p>Bolo fácil, rápido e prático</p>
+    </Link>
   );
 }
