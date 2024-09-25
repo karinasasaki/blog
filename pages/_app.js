2968330993a073a2324157d81a5309be125c7945
Karina Sasaki <karinasasaki99@gmail.com>
2024-09-24 20:50:13 -0400

    feat: :sparkles: Adiciona Font Awesome no projeto

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
   return (
