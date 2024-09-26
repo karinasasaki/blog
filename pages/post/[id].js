ed4ee6f84a408b8ef617a03db9de23e8db0fb0bc
Karina Sasaki <karinasasaki99@gmail.com>
2024-09-26 12:08:40 -0400

    feat: :sparkles: Cada post traz informações completas

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,15 +1,54 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Post() {
   const router = useRouter();
+  const { id } = router.query;
+  const [receita, setReceita] = useState();
+
+  const fetchData = async() => {
+    try {
+      const response = await fetch(`http://localhost:3001/receitas/${id}`, {
+        header: {
+          Accept: "application/json",
+          method: "GET"
+        }
+      });
+      if (response) {
+        const data = await response.json();
+        setReceita(data);
+      }
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <Head>
-        <title>Panelinha - Post</title>
+        <title>Panelinha - {receita?.titulo}</title>
       </Head>
-      <h1>Post id: {router.query.id}</h1>
+      <h1>{receita?.titulo}</h1>
+      <p>Data da postagem: {receita?.data}</p>
+      <p>Autor(a): {receita?.autor}</p>
+      <h2>Ingredientes:</h2>
+      <ul>
+        {receita?.ingredientes.map(ingrediente => {
+          return <li key={ingrediente}>{ingrediente}</li>
+        })}
+      </ul>
+      <h2>Modo de preparo:</h2>
+      <ol>
+        {receita?.comoFazer.map(modo => {
+          return <li key={modo}>{modo}</li>
+        })}
+      </ol>
       <Link href={"/"}>Voltar</Link>
     </>
   );
